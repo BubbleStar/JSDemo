5e73944d1c63f9219cc0eaad5a6268133c11d8c4
unknown <3358136292@qq.com>
2016-09-19 15:28:35 +0800

    upload bind方法

---
+++ 一些算法/实现bind方法/正常版bind.js
@@ -0,0 +1,18 @@
+﻿Function.prototype.bind = function(){
+	var self = this; //保存原函数
+	var context = [].shift.apply(arguments); //保存要绑定的对象
+	var args = [].slice.call(arguments);
+	return function(){
+		return self.apply(context,[].concat.apply(args,[].slice.call(arguments)));
+	}
+}
+
+var obj = {
+	name : 'bubble'
+}
+
+var func = function(a,b,c,d){
+	alert(this.name + a + b + c + d);
+}.bind(obj,1,2);
+
+func(3,4); //输出：bubble1234
---
+++ 一些算法/实现bind方法/简化版bind.js
@@ -0,0 +1,16 @@
+﻿Function.prototype.bind = function(context){
+	var self = this;	//保存原函数
+	return function(){	//返回一个新函数
+		return self.apply(context,arguments);
+	}
+}
+
+var obj = {
+	name : 'bubble';
+}
+
+var func = function(){
+	alert(this.name);
+}.bind(obj)
+
+func(1,2); //输出:bubble12
